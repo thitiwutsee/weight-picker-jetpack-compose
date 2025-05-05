7780dcf9c7167796bebbf3c65f4b162e14cc09c6
Thitiwut <thitiwutsee12@gmail.com>
2025-05-05 20:26:37 +0700

    feat: Configure Compose inspection settings and update scale component usage
    This commit introduces the configuration for Compose-related inspections and refactors the usage of the `Scale` component in `MainActivity`.
    
    Key changes:
    
    -   **Inspection Profiles:**
        - Added `Project_Default.xml` to `.idea/inspectionProfiles/` to manage Compose inspection settings.
        - Enabled various inspection rules for Compose, such as preview function requirements, dimension limits, API level, font scale, parameter providers, and annotation usage.
    -   **MainActivity:**
        -   Refactor to use `Scale` composable in `MainContent` function.
        -   Change `fillMaxSize` to `fillMaxWidth` and set height 300dp in the `Scale` composable.
        -   Implement `weight` variable to receive the weight from the `Scale`.
        - Remove `Preview` composable.

--- app/src/main/java/com/thitiwut/weightpicker/MainActivity.kt
+++ app/src/main/java/com/thitiwut/weightpicker/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.thitiwut.weightpicker.ui.theme.WeightPickerTheme
 
@@ -18,34 +21,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
+            var weight by remember {
+                mutableIntStateOf(80)
+            }
             WeightPickerTheme {
-                MainContent()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Scale(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(300.dp)
+                            .align(Alignment.BottomCenter),
+                        style = ScaleStyle(
+                            scaleWidth = 150.dp
+                        )
+                    ) {
+                        weight = it
+
+                    }
+                }
             }
         }
-    }
-}
-
-@Composable
-fun MainContent() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Scale(
-            modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.Center),
-            style = ScaleStyle(
-                scaleWidth = 150.dp
-            )
-        ) {
-
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeightPickerTheme {
-        MainContent()
     }
 }
